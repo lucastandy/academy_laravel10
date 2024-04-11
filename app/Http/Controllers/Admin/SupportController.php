771c07adbc01fc606e1ac76301803b2db28c099f
lucastandy <lucastitandy@gmail.com>
2024-04-11 19:59:54 -0300

    Atualizando registro

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -39,4 +39,27 @@ class SupportController extends Controller
         return redirect()->route('supports.index');
 
     }
+
+    public function edit(Support $support, string|int $id){
+        // Verifica se o id existe. Se não existir, o usuário será redirecionado para a página anterior
+        if(!$support = $support->where('id', $id)->first()){
+            return back();
+        }
+
+        return view('admin/supports.edit',compact('support'));
+    }
+
+    public function update(Request $request, Support $support, string $id){
+        
+        if(!$support = $support->find($id)){
+            return back();
+        }
+
+        $support->update($request->only([
+            'subject','body'
+        ]));
+
+        return redirect()->route('supports.index');
+    }
+
 }
