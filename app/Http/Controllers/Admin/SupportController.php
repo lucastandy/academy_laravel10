7408f3760f0e0f1a41f5ac5eb836e028bdbb61e3
lucastandy <lucastitandy@gmail.com>
2024-04-13 10:38:04 -0300

    Validações do jeito certo

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
 use Illuminate\Http\Request;
 
@@ -29,7 +30,7 @@ class SupportController extends Controller
 
 
 
-    public function store(Request $request, Support $support){
+    public function store(StoreUpdateSupport $request, Support $support){
 
         $data = $request->all();
         $data['status'] = 'a';
