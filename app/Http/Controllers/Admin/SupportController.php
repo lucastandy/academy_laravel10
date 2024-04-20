e3d63114169fbf2e5068f09f531fd3f955946472
lucastandy <lucastitandy@gmail.com>
2024-04-20 11:25:58 -0300

    Validando valor único no update com laravel 10

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -32,7 +32,7 @@ class SupportController extends Controller
 
     public function store(StoreUpdateSupport $request, Support $support){
 
-        $data = $request->all();
+        $data = $request->validated();
         $data['status'] = 'a';
 
         $support = $support->create($data);
@@ -50,15 +50,13 @@ class SupportController extends Controller
         return view('admin/supports.edit',compact('support'));
     }
 
-    public function update(Request $request, Support $support, string $id){
+    public function update(StoreUpdateSupport $request, Support $support, string $id){
         
         if(!$support = $support->find($id)){
             return back();
         }
 
-        $support->update($request->only([
-            'subject','body'
-        ]));
+        $support->update($request->validated());
 
         return redirect()->route('supports.index');
     }
