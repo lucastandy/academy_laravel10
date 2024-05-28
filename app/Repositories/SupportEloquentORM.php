58a0edb7dd763b3a42b48809f633e98e200edd6c
lucastandy <lucastitandy@gmail.com>
2024-05-27 23:12:55 -0300

    Criando Componente de Paginação Personalizado

--- app/Repositories/SupportEloquentORM.php
+++ app/Repositories/SupportEloquentORM.php
@@ -21,7 +21,7 @@ class SupportEloquentORM implements SupportRepositoryInterface{
             }
 
         })->paginate($totalPerPage, ['*'],'page',$page);
-        dd((new PaginationPresenter($result))->items());
+        // dd((new PaginationPresenter($result))->items());
         return new PaginationPresenter($result);
     }
 
